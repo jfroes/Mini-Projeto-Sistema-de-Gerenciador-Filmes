9bba6be0a60505775aa50d98c9becf24231a1550
José Froes <josepaulo.froes@gmail.com>
2025-12-12 12:16:29 -0300

    Implemented App menu

--- src/App.java
+++ src/App.java
@@ -1,18 +1,39 @@
-import dto.FilmeDTO;
-import repository.FilmeRepository;
-import services.FilmeService;
-
-import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        FilmeService service = new FilmeService();
-        FilmeRepository repository = new FilmeRepository();
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        boolean sair = false;
+
+        System.out.println(menu());
+
+        do {
+
+            int opcao = sc.nextInt();
+
+            sair = opcao == 0 ? true : false;
+
+        }while (sair == false);
 
 
+        sc.close();
 
-       List<FilmeDTO> filmesTitulo = service.findByDiretor("la");
+    }
 
-       filmesTitulo.forEach(System.out::println);
+    public static String menu(){
+        return """
+                :::CÁTALOGO DE FILMES:::
+                ------------------------
+               \s
+                DIGITE UMA OPÇÃO:
+                1 - LISTAR FILMES
+                2 - PESQUSISAR FILME
+                3 - CADASTRAR FILME
+                4 - DELETAR FILME
+                0 - SAIR             \s
+               \s""";
     }
 }
