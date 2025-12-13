1d1852131a9bfce4244009e73b9905085b8c2536
José Froes <josepaulo.froes@gmail.com>
2025-12-12 22:46:17 -0300

    Controller layer for interactions between app and service layer

--- src/controllers/FilmeController.java
+++ src/controllers/FilmeController.java
@@ -2,7 +2,9 @@ package controllers;
 
 import dto.FilmeDTO;
 import entities.Filme;
+import exceptions.DuplicatedResourceException;
 import exceptions.ResourceNotFoundExeception;
+import resources.Mensagens;
 import services.FilmeService;
 
 import java.util.Comparator;
@@ -11,6 +13,24 @@ import java.util.List;
 public class FilmeController {
 
     FilmeService service = new FilmeService();
+
+    public String addFilme(String titulo, String genero, String diretor, int ano, double nota){
+        try{
+            FilmeDTO dto = new FilmeDTO(0, titulo, genero, diretor, ano, nota);
+            return  service.insert(dto);
+        }catch (DuplicatedResourceException e){
+            return Mensagens.erro(Mensagens.erro(e.getMessage()));
+        }
+    }
+
+    public void deleteFilme(int id){
+        try{
+            service.deleteFilme(id);
+            System.out.println(Mensagens.sucesso("FILME DELETADO COM SUCESSO."));
+        }catch (ResourceNotFoundExeception e){
+            System.out.println(Mensagens.erro(Mensagens.erro(e.getMessage())));
+        }
+    }
 
     public void findAll(){
         List<FilmeDTO> filmes= service.findAll();
@@ -22,7 +42,7 @@ public class FilmeController {
             List<FilmeDTO> filmes = service.findByTitulo(titulo).stream().sorted(Comparator.comparing(FilmeDTO::getTitulo)).toList();
             filmes.forEach(System.out::println);
         } catch (ResourceNotFoundExeception e) {
-            System.out.println(e.getMessage());
+            System.out.println(Mensagens.erro(e.getMessage()));
         }
     }
 
@@ -31,7 +51,7 @@ public class FilmeController {
             List<FilmeDTO> filmes = service.findByGenero(genero).stream().sorted(Comparator.comparing(FilmeDTO::getGenero)).toList();
             filmes.forEach(System.out::println);
         }catch (ResourceNotFoundExeception e){
-            System.out.println(e.getMessage());
+            System.out.println(Mensagens.erro(e.getMessage()));
         }
     }
 
@@ -40,7 +60,7 @@ public class FilmeController {
             List<FilmeDTO> filmes = service.findByDiretor(diretor).stream().sorted(Comparator.comparing(FilmeDTO::getTitulo)).toList();
             filmes.forEach(System.out::println);
         }catch (ResourceNotFoundExeception e ){
-            System.out.println(e.getMessage());
+            System.out.println(Mensagens.erro(e.getMessage()));
         }
     }
 
@@ -49,7 +69,7 @@ public class FilmeController {
             List<FilmeDTO> filmes = service.findByAno(ano).stream().sorted(Comparator.comparing(FilmeDTO::getAno)).toList();
             filmes.forEach(System.out::println);
         }catch (ResourceNotFoundExeception e ){
-            System.out.println(e.getMessage());
+            System.out.println(Mensagens.erro(e.getMessage()));
         }
     }
 }
